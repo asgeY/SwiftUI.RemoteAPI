197c13aea94618a43b9071b1bd086313a926f5e7
Simran Preet Singh <simran5590@gmail.com>
2019-08-23 08:01:52 -0700

    Initial Commit

---
+++ RemoteAPISwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  RemoteAPISwiftUI
+//
+//  Created by Simran Preet Narang on 8/23/19.
+//  Copyright © 2019 Simran Preet Narang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
